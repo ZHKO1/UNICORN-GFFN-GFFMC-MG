beae8b6aa4424df2746309687c115c20792503a8
ZHKO1 <zhko11993@gmail.com>
2017-10-04 17:30:44 +0800

    update SideBar

--- src/components/appSidebar.vue
+++ src/components/appSidebar.vue
@@ -1,26 +1,39 @@
 <template>
   <div class="appSideBar">
-    <ul>
-      <li v-for="item in SideLi">{{item.name}}<i class="fa fa-check"></i></li>
-    </ul>
+    <div class="switchSideBar" style="">
+      <i class="fa fa-angle-right"></i>
+    </div>
+    <div class="main">
+      <ul>
+        <li v-for="item in SideLi" @click="toggleLi(item.id)">{{item.name}}<i class="fa" :class="{'fa-check' : item.show}"></i></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
   name: 'appSideBar',
   data () {
     return {
       SideLi: [
-        {id:0, name:"FIX G.F.F.N ArtWork"},
-        {id:1, name:"FIX G.F.F.N Toy"},
+        {id:0, name:"FIX G.F.F.N ArtWork", show: true},
+        {id:1, name:"FIX G.F.F.N Toy", show: true},
         {id:2, name:"FIX MC  ArtWork"},
         {id:3, name:"FIX MC  Toy"},
         {id:4, name:"MG Ver.Ka  ArtWork"},
         {id:5, name:"MG Ver.Ka  Toy"},
       ]
     }
-  }
+  },
+  methods: {
+    toggleLi(id) {
+      var that = this;
+      Vue.set(that.SideLi[id], "show", that.SideLi[id].show? (!that.SideLi[id].show): true);
+    }
+  },
 }
 </script>
 
@@ -28,45 +41,68 @@ export default {
 <style scoped lang="less">
 @import "./../lib/style/font-awesome/css/font-awesome.css";
 .appSideBar{
-  position: fixed;
-  width: 250px;
-  height: 80vh;
-  background: rgba(255, 255, 255, 0.8);
-  left: 0px;
-  font-family: 'Lucida Grande','Hiragino Kaku Gothic Pro','ヒラギノ角ゴ Pro W3','ＭＳ Ｐゴシック', sans-serif;
-  font-weight: normal;
-  color: #222;
-  z-index:2;
-  ul{
-    clear: left;
-    margin-left: 1em;
-    padding: 0 0 1px;
-    li{
-      position: relative;
-      user-select: none;
-      -moz-user-select: none;
-      -webkit-user-select: none;
-      cursor: default;
+  .switchSideBar{
+    background-color: #58b7ff;
+    position: fixed;
+    left: 50px;
+    top: 150px;
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+    cursor: pointer;
+    opacity: .4;
+    transition: .3s;
+    i{
+      color: #fff;
       display: block;
-      border: 1px solid black;
-      background: white;
-      margin: 0;
-      padding: 0 1em;
-      border-radius: 3px;
-      margin-bottom: -1px;
-      max-width: 100%;
-      line-height: 3;
-      vertical-align: middle;
-      i{
-        position: absolute;
-        width: 35px;
+      line-height: 50px;
+      text-align: center;
+      font-size: 2.5em;
+    }
+  }
+
+  .main{
+    display: none;
+    position: fixed;
+    width: 280px;
+    height: 80vh;
+    background: rgba(255, 255, 255, 0.8);
+    left: 0px;
+    font-family: 'Lucida Grande','Hiragino Kaku Gothic Pro','ヒラギノ角ゴ Pro W3','ＭＳ Ｐゴシック', sans-serif;
+    font-weight: normal;
+    color: #222;
+    z-index:2;
+    ul{
+      clear: left;
+      margin: 1em;
+      padding: 0 0 1px;
+      li{
+        position: relative;
+        user-select: none;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        cursor: default;
+        display: block;
+        border: 1px solid black;
+        background: white;
+        margin: 0;
+        padding: 0 1em;
+        border-radius: 3px;
+        margin-bottom: -1px;
+        max-width: 100%;
         line-height: 3;
-        right: 0;
-        top: 0;
-        text-align: center;
-        color: #222;
-        transition: all .3s;
+        vertical-align: middle;
         cursor: pointer;
+        i{
+          position: absolute;
+          width: 35px;
+          line-height: 3;
+          right: 0;
+          top: 0;
+          text-align: center;
+          color: #222;
+          transition: all .3s;
+        }
       }
     }
   }
